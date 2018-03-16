0a82f11748282a383631eef0001ef099046ab6bf
keyth202 <combs.cdi@gmail.com>
2018-03-16 13:43:16 -0400

    Changed PORT process

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-const port = 5000;
 
 
 app.get('/', (req,res) => {
 	res.send({hi:'There'});
-}); 
+});
 
-app.listen(port, () => console.log('Example app listening on port 5000!'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Example app listening on port 5000!'));
