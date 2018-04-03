878430555b18e0589446fa15bd0ed3ec9c083b01
keyth202 <combs.cdi@gmail.com>
2018-04-03 15:19:35 -0400

    Finish Auth flow

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+require('./models/user');
+require('./services/passport.js');
+
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
+app.use(
+	cookieSession({
+		maxAge:30*24*60*60*1000,
+		keys: [keys.cookieKey]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 
-app.get('/', (req,res) => {
-	res.send({hi:'There'});
-});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Example app listening on port 5000!'));
+app.listen(PORT, () => console.log('Example app listening on port 5000!')); 
+
+
---
+++ routes/authRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+
+module.exports = app => {
+
+	app.get('/auth/google', passport.authenticate('google', {
+			scope: ['profile', 'email']
+		})
+	);
+
+	app.get('/auth/google/callback', passport.authenticate('google'));
+
+	app.get('/api/logout', (req,res)=> {
+		req.logout();
+		res.send(req.user);
+	});
+
+	app.get('/api/current_user', (req,res) => {
+		res.send(req.user);
+	});
+
+};
